76e852de0420b9dc673bdc5deca3e005b340a02d
keneilwe <keraetswe.kc@gmail.com>
2022-03-11 15:10:39 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
