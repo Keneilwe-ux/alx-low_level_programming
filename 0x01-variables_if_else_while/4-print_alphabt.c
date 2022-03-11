0b649415cd4eb1f5014309ff264091a66a41946e
keneilwe <keraetswe.kc@gmail.com>
2022-03-11 15:35:21 -0800

    Print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+			ch++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
